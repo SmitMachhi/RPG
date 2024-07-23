abbd241c0491edd306316898440e526f5726b707
smitmachhi <thesmitmachhi@gmail.com>
2024-07-23 11:51:57 -0400

    chapter function

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-let lore = $("#lore");
+const lore = $("#lore");
 
+const start = $("#start");
 const btn1 = $("#btn1");
 const btn2 = $("#btn2");
 
-let playerStats = { xp: 10, hp: 100, gold: 20 };
+let playerStats = { xp: 0, hp: 100, gold: 20 };
 
 $.each(playerStats, function (stat, value) {
   $("#" + stat).text(value);
 });
+
+$(document).ready(cssShow(start), cssHide(btn1), cssHide(btn2));
+
+function cssHide(element) {
+  $(element).css("display", "none");
+}
+
+function cssShow(element) {
+  $(element).css("display", "block");
+}
 
 const story = {
   chapter1: {
@@ -30,13 +41,23 @@ const story = {
   // Additional chapters can be added here
 };
 
-$(document).ready(function () {
-  function cssHide(element) {
-    $(element).css("display", "none");
-  }
-  cssHide(btn2);
+function updateXp(amount) {
+  playerStats.xp += amount;
+  return amount;
+}
+
+function updateXpUI() {
+  $("#xp").html(playerStats.xp);
+}
+
+start.click(function () {
+  cssHide(start);
+  chapter1();
 });
 
-btn1.click(function () {
-  $("#xp").html(playerStats.xp);
-});
+function chapter1() {
+  lore.html(story.chapter1.line).slideDown(2000);
+  setInterval((cssShow(btn1), cssShow(btn2)), 3000);
+  btn1.text(story.chapter1.choices[0].text);
+  btn2.text(story.chapter1.choices[1].text);
+}
