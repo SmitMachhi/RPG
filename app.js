a83ca08926f8e313a0942d9eee5f329f8e67bfc6
smitmachhi <thesmitmachhi@gmail.com>
2024-07-24 00:17:29 -0400

    new buttons, stat update function

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const btn2 = $("#btn2");
 
 let playerStats = { xp: 0, hp: 100, gold: 20 };
 
-$.each(playerStats, function (stat, value) {
-  $("#" + stat).text(value);
+$(".points").each(function () {
+  let statId = $(this).attr("id"); // Get the id of the current element
+  $(this).text(playerStats[statId]); // Set the text content to the corresponding value in playerStats
 });
 
 $(document).ready(cssShow(start), cssHide(btn1), cssHide(btn2));
@@ -28,41 +29,64 @@ const story = {
     choices: [
       {
         text: "Stay in Eldoria",
-        outcome: "endgame",
-        effects: { xp: 10, hp: 0, money: 0 },
+        // outcome: "endGame",
+        effects: { xp: 0, hp: 0, gold: 0 },
       },
       {
         text: "Embark on the Journey",
-        outcome: "chapter2",
-        effects: { xp: 50, hp: 0, money: 0 },
+        // outcome: "chapter2",
+        effects: { xp: 50, hp: -10, gold: 20 },
       },
     ],
   },
   // Additional chapters can be added here
 };
 
-function updateXp(amount) {
-  playerStats.xp += amount;
-  return amount;
-}
-
-function updateXpUI() {
-  $("#xp").html(playerStats.xp);
-}
-
 start.click(function () {
   cssHide(start);
   chapter1();
 });
 
+/* function updateXpUI() {
+  $("#xp").html(playerStats.xp);
+} */
+
+function updateStats(v1, v2, v3) {
+  playerStats.xp += v1;
+  playerStats.hp += v2;
+  playerStats.gold += v3;
+
+  $("#xp").html(playerStats.xp);
+  $("#hp").html(playerStats.hp);
+  $("#gold").html(playerStats.gold);
+}
+
 function chapter1() {
   lore.html(story.chapter1.line).slideDown(2000);
 
-  setInterval(function () {
-    cssShow(btn1);
-    cssShow(btn2);
-  }, 5000);
+  setTimeout(function () {
+    btn1.fadeIn(1000); // Fade in over 1 second
+    btn2.fadeIn(1000); // Fade in over 1 second
+  }, 3000);
 
   btn1.text(story.chapter1.choices[0].text);
   btn2.text(story.chapter1.choices[1].text);
+
+  btn1.click(function () {
+    endGame();
+    $(".button").attr("disabled", true);
+  });
+
+  btn2.click(function () {
+    updateStats(
+      story.chapter1.choices[1].effects.xp,
+      story.chapter1.choices[1].effects.hp,
+      story.chapter1.choices[1].effects.gold
+    );
+    $(".button").attr("disabled", true);
+  });
+}
+
+function endGame() {
+  $(".container").css("background-color", "red");
 }
